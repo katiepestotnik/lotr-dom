8aa14a5a5f7da62d17e8dd621d2bcde76ece0c35
katiepestotnik <katie.pestotnik@gmail.com>
2021-05-25 17:21:58 -0600

    Chapter 1 complete - Made Middle Earth

--- js/app.js
+++ js/app.js
@@ -38,23 +38,24 @@ function makeMiddleEarth() {
   console.log("1: makeMiddleEarth");
 
   // 1. create a section tag with an id of middle-earth
+  const middleEarth = document.createElement("section");
+  middleEarth.setAttribute("id", "middle-earth");
 
   // 2. use a for loop to iterate over the lands array that does the following:
-
+  for (let index = 0; index < lands.length; index++) {
   //   2a. creates an article tag (there should be one for each land when the loop is done)
+      const article = document.createElement("article");
+   //   2b. gives each land article an `id` tag of the corresponding land name
+      article.setAttribute("id", lands[index]);
+   //   2c. includes an h1 with the name of the land inside each land article
+      article.innerHTML = `<h1>${lands[index]}</h1>`
+ //   2d. appends each land to the middle-earth section
+      middleEarth.appendChild(article);
+  }
 
-  //   2b. gives each land article an `id` tag of the corresponding land name
-
-  //   2c. includes an h1 with the name of the land inside each land article
-
-  //   2d. appends each land to the middle-earth section
-
-  // 3. append the section to the body of the DOM.
-
+// 3. append the section to the body of the DOM.
+document.body.appendChild(middleEarth);
 }
-
-
-
 
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 1 complete - Made Middle Earth".
